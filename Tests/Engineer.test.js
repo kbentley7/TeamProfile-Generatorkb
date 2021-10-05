3558271ce9e7da5025cbaea82b34ff7b93ae1c02
Kenneth E. Bentley, Sr <kenmo8466@gmail.com>
2021-10-05 18:13:51 -0400

    edited Engineer.test.js and Intern.test.js

--- Tests/Engineer.test.js
+++ Tests/Engineer.test.js
@@ -1,12 +1,12 @@
 const Engineer = require("../lib/Engineer");
 
-test("", () => {
+test("Using a constructor function to set GitHub user name", () => {
     const github = "kbentley7";
     const employee = new Engineer("Kenneth", 1, "kenmo8466@gmail.com", github);
     expect(employee.github).toBe(github);
   });
 
-  test("getRole() should return Engineer as a role", () => {
+  test("getRole() A role of Engineer should be retruned", () => {
     const role = "Engineer";
     const employee = new Engineer("Kenneth", 1, "kenmo8466@gmail.com", "kennethbentey");
     expect(employee.getRole()).toBe(role);
--- Tests/intern.test.js
+++ Tests/intern.test.js
@@ -1,12 +1,12 @@
 const Intern = require("../lib/Intern");
 
-test("Using a constructor function to set GitHub username ", () => {
+test("Using a constructor function to set school ", () => {
     const school = "Shoalin School of Martial Arts";
     const employee = new Intern("Kenneth", 1, "kenmo8466@gmail.com", school);
     expect(employee.school).toBe(school);
   });
 
-  test("getRole() should return Intern as a role", () => {
+  test("getRole()  A role of Intern should be retruned", () => {
     const role = "Intern";
     const employee = new Intern("Kenneth", 1, "kenmo8466@gmail.com", "kennethbentley");
     expect(employee.getRole()).toBe(role);
