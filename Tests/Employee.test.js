9bb83c6856c0f95a54a32348c398664ff9d13f90
Kenneth E. Bentley, Sr <kenmo8466@gmail.com>
2021-10-05 17:06:46 -0400

    edited Employee.test.js

--- Tests/Employee.test.js
+++ Tests/Employee.test.js
@@ -1,25 +1,25 @@
 const Employee = require("../lib/Employee");
 
 test("Ability to set name using constructor function", () => {
-    const name = "Michael Scott";
+    const name = "FakeBruce Lee";
     const employee = new Employee(name);
     expect(employee.name).toBe(name);
   });
 
 test("Ability to set the user ID using constructor function", () => {
     const id = "1";
-    const employee = new Employee("Michael Scott", id, "michael.scott@dundermifflin.com");
+    const employee = new Employee("FakeBruce Lee", id, "fakebruce.lee@gmail.com");
     expect(employee.id).toBe(id);
   });
 
 test("Running getEmail() should return the supplied email", () => {
-    const email = "michael.scott@dundermifflin.com";
-    const employee = new Employee("Michael Scott", 1, email);
+    const email = "Fakebruce.lee@gmail.com";
+    const employee = new Employee("FakeBruce Lee", 1, email);
     expect(employee.getEmail()).toBe(email);
   });
 
 test("Running getRole() should return 'Employee'", () => {
     const role = "Employee";
-    const employee = new Employee("Michael Scott", 1, "michael.scott@dundermifflin.com");
+    const employee = new Employee("FakeBruce Lee", 1, "fakebruce.lee@gmail.com");
     expect(employee.getRole()).toBe(role);
   });
